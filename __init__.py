4149bdbc167687a4c61480c0d7f8d0121c049174
Kris Gesling <kris.gesling@mycroft.ai>
2021-10-27 11:50:28 +0930

    Add sleep/wake face from Mark II Skill
    This moves the sleep/wake face from the Mark II Skill into the
    naptime Skill where the actions are performed.
    
    It also includes a refactor to tidy up and segregate actions based
    on the platform they are running on.

--- __init__.py
+++ __init__.py
@@ -12,28 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from adapt.intent import IntentBuilder
+import time
 
-from mycroft import MycroftSkill, intent_handler
-from mycroft.messagebus.message import Message
+from mycroft import AdaptIntent, MycroftSkill, intent_handler
 from mycroft.audio import wait_while_speaking
 from mycroft.configuration.config import Configuration
-
-import time
+from mycroft.messagebus.message import Message
 
 
 class NapTimeSkill(MycroftSkill):
     """Skill to handle mycroft speech client listener sleeping."""
-    def initialize(self):
+
+    def __init__(self) -> None:
+        """Initialize NapTimeSkill"""
+        super().__init__()
         self.started_by_skill = False
         self.sleeping = False
         self.old_brightness = 30
-        self.add_event('mycroft.awoken', self.handle_awoken)
-        self.wake_word = Configuration.get()['listener']['wake_word']
         self.disabled_confirm_listening = False
 
-    @intent_handler(IntentBuilder("NapTimeIntent").require("SleepCommand"))
-    def handle_go_to_sleep(self, message):
+    def initialize(self) -> None:
+        """Perform final initialization once Skill class has loaded."""
+        self.add_event('mycroft.awoken', self.handle_awoken)
+        self.platform = self.config_core.get(
+            "enclosure").get("platform", "unknown")
+        self.wake_word = Configuration.get()['listener']['wake_word']
+
+    @intent_handler(AdaptIntent("NapTimeIntent").require("SleepCommand"))
+    def handle_go_to_sleep(self, _) -> None:
         """Sends a message to the speech client putting the listener to sleep.
 
         If the user has been told about the waking up process five times
@@ -47,29 +53,9 @@ class NapTimeSkill(MycroftSkill):
             self.speak_dialog('going.to.sleep', {'wake_word': self.wake_word})
         else:
             self.speak_dialog('going.to.sleep.short')
+        self.perform_sleep()
 
-        self.bus.emit(Message('recognizer_loop:sleep'))
-        self.sleeping = True
-        self.started_by_skill = True
-        wait_while_speaking()
-        time.sleep(2)
-        wait_while_speaking()
-
-        # Dim and look downward to 'go to sleep'
-        # TODO: Get current brightness from somewhere
-        self.old_brightness = 30
-        for i in range(0, (self.old_brightness - 10) // 2):
-            self.enclosure.eyes_brightness(self.old_brightness - i * 2)
-            time.sleep(0.15)
-        self.enclosure.eyes_look("d")
-        platform = self.config_core.get("enclosure").get("platform", "unknown")
-        if platform != "unknown":
-            self.bus.emit(Message('mycroft.volume.mute',
-                                  data={"speak_message": False}))
-        elif self.config_core['confirm_listening']:
-            self.disable_confirm_listening()
-
-    def handle_awoken(self, message):
+    def handle_awoken(self, _) -> None:
         """Handler for the mycroft.awoken message
 
         The message is sent when the listener hears 'Hey Mycroft, Wake Up',
@@ -77,14 +63,40 @@ class NapTimeSkill(MycroftSkill):
         """
         started_by_skill = self.started_by_skill
 
-        self.awaken()
+        self.perform_awaken()
         if started_by_skill:
-            self.wake_up_animation()
             # Announce that the unit is awake
             self.speak_dialog("i.am.awake")
-            wait_while_speaking()
+            self.display_waking_face()
 
-    def wake_up_animation(self):
+    def display_sleep_face(self) -> None:
+        """Display the sleeping face depending on the platform."""
+        if self.gui.connected:
+            self.gui.show_page("resting.qml", override_idle=True)
+        elif self.platform == "mycroft_mark_1":
+            self.display_sleep_animation()
+
+    def display_sleep_animation(self) -> None:
+        """Dim and look downward to 'go to sleep'."""
+        # TODO: Get current brightness from somewhere
+        self.old_brightness = 30
+        for i in range(0, (self.old_brightness - 10) // 2):
+            self.enclosure.eyes_brightness(self.old_brightness - i * 2)
+            time.sleep(0.15)
+        self.enclosure.eyes_look("d")
+
+    def display_waking_face(self) -> None:
+        """Display the waking face depending on the platform."""
+        if self.gui.connected:
+            self.gui.remove_page("resting.qml")
+            self.gui.show_page("awake.qml", override_idle=5)
+            # TODO this shouldn't be necessary - remove 2 lines when fixed.
+            time.sleep(5)
+            self.gui.release()
+        elif self.platform == 'mycroft_mark_1':
+            self.display_wake_up_animation()
+
+    def display_wake_up_animation(self) -> None:
         """Mild animation to come out of sleep from voice command.
 
         Pop open eyes and wait a sec.
@@ -96,9 +108,9 @@ class NapTimeSkill(MycroftSkill):
         # brighten the rest of the way
         self.enclosure.eyes_brightness(self.old_brightness)
 
-    def awaken(self):
-        platform = self.config_core.get("enclosure").get("platform", "unknown")
-        if platform != "unknown":
+    def perform_awaken(self) -> None:
+        """Perform actions to wake system up."""
+        if self.platform != "unknown":
             self.bus.emit(Message('mycroft.volume.unmute',
                                   data={"speak_message": False}))
         elif self.disabled_confirm_listening:
@@ -107,21 +119,40 @@ class NapTimeSkill(MycroftSkill):
         self.sleeping = False
         self.started_by_skill = False
 
-    def disable_confirm_listening(self):
+    def perform_sleep(self) -> None:
+        """Perform actions to put system to sleep."""
+        self.bus.emit(Message('recognizer_loop:sleep'))
+        self.sleeping = True
+        self.started_by_skill = True
+        # TODO - Work out why this is here...
+        wait_while_speaking()
+        time.sleep(2)
+        wait_while_speaking()
+        self.display_sleep_face()
+        if self.platform != "unknown":
+            self.bus.emit(Message('mycroft.volume.mute',
+                                  data={"speak_message": False}))
+        elif self.config_core['confirm_listening']:
+            self.disable_confirm_listening()
+
+    def disable_confirm_listening(self) -> None:
+        """Patch active mycroft configuration to disable listening beep."""
         msg = Message('configuration.patch',
                       data={'config': {'confirm_listening': False}}
                       )
         self.bus.emit(msg)
         self.disabled_confirm_listening = True
-        self.log.info('Disabled chirp')
+        self.log.info('Disabled listening chirp')
 
-    def enable_confirm_listening(self):
+    def enable_confirm_listening(self) -> None:
+        """Patch active mycroft configuration to enable listening beep."""
         msg = Message('configuration.patch',
                       data={'config': {'confirm_listening': True}}
                       )
         self.bus.emit(msg)
         self.disabled_confirm_listening = False
-        self.log.info('Enabled chirp again')
+        self.log.info('Enabled listening chirp again')
+
 
 def create_skill():
     return NapTimeSkill()
